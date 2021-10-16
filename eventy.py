981d0f8f5eb0e4ccfd15250415afa59dd046cf5c
Ayushman Khazanchi <ayushman.khazanchi@gmail.com>
2021-10-17 00:58:10 +0200

    debugging change for Request table in flask shell

--- eventy.py
+++ eventy.py
@@ -1,8 +1,8 @@
 'from the app package import the app instance variable'
 from app import app, db
-from app.models import User, Post
+from app.models import User, Request
 
 'useful for debugging, define shell constants when using `flask shell` for debug'
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'Post': Post}
+    return {'db': db, 'User': User, 'Request': Request}
